cd7e12e91dcde7a64f7473c7bb87e006baf518ba
EmmanuelAcosta <emmanuel.acosta@correo.ucu.edu.uy>
2021-10-11 13:10:47 -0300

    Se mandan cambios

---
+++ docs/index.js
@@ -0,0 +1,31 @@
+var form = document.getElementById("formulario");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+var a= 0;
+var listUser = [];
+
+function Persona(nombre,apellido,correo,pais,empresa,comentarios){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.correo = correo;
+    this.pais = pais;
+    this.empresa = empresa;
+    this.comentarios = comentarios;
+}
+
+function mostrarPersonas(){
+    alert(listUser);
+    
+}
+
+function agregarPersona(){
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var pais = document.getElementById("pais").value;
+    var empresa = document.getElementById("empresa").value;
+    var comentarios = document.getElementById("comentarios").value;
+    var persona = new Persona(nombre,apellido,pais,empresa,comentarios).value;
+    // ¡Cambiar y poner persona!
+    listUser.push(nombre + apellido + empresa);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,25 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   variants: {
-    extend: {},
+    extend: { textColor: ['visited'],},
   },
   plugins: [
     require('@tailwindcss/typography')
