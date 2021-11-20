9c55434798841ee6a7625bd4a45bd579d9c2f0b3
EmmanuelAcosta <emmanuel.acosta@correo.ucu.edu.uy>
2021-11-20 17:24:42 -0300

    se saca codigo  basura

--- src/script.js
+++ src/script.js
@@ -1,34 +1,14 @@
+let nombreSc = document.getElementById("nombre");
+let apellidoSc = document.getElementById("apellido");
+let emailSc = document.getElementById("email");
+let paisSc = document.getElementById("pais");
+let comentariosSc = document.getElementById("comentarios");
 var form = document.getElementById("formulario");
 function handleForm(event) {
   event.preventDefault();
 }
 if (form != null) {
   form.addEventListener("submit", handleForm);
-}
-
-var a = 0;
-var listUser = [];
-
-class Persona {
-  nombre
-  apellido
-  correo
-  pais
-  empresa
-  comentarios
-  constructor(nombre, apellido, correo, pais, empresa, comentarios) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.correo = correo;
-    this.pais = pais;
-    this.empresa = empresa;
-    this.comentarios = comentarios;
-
-  }
-}
-
-function mostrarPersonas() {
-  alert(listUser);
 }
 
 let botonContacto = document.getElementById("open-btn");
@@ -50,22 +30,9 @@ function setUpButtons() {
   }
 
 }
-
-function agregarPersona() {
-  var nombre = document.getElementById("nombre").value;
-  var apellido = document.getElementById("apellido").value;
-  var pais = document.getElementById("pais").value;
-  var correo = document.getElementById("correo").value;
-  var empresa = document.getElementById("empresa").value;
-  var comentarios = document.getElementById("comentarios").value;
-  var persona = new Persona(nombre, apellido,correo, pais, empresa, comentarios);
-  // ¡Cambiar y poner persona!
-  listUser.push(nombre + apellido + empresa);
-  cerrarModal();
-}
-
 function cerrarModal() {
   modal?.classList.add("esconder-modal");
+  limpiarCampos();
 }
 
 function abrirModal() {
@@ -73,5 +40,9 @@ function abrirModal() {
 }
 
 function limpiarCampos() {
-  console.log("CAMPOS A LIMPIAR")
+  nombreSc.value = "";
+  apellidoSc.value = "";
+  comentariosSc.value = "";
+  emailSc.value = "";
+  paisSc.value = "";
 }
