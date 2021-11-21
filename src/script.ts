fbb6837c4884ecc727516cb8c46f33927d33bb22
EmmanuelAcosta <emmanuel.acosta@correo.ucu.edu.uy>
2021-11-20 21:12:08 -0300

    cambios de estilos

---
+++ src/script.ts
@@ -0,0 +1,48 @@
+let nombreSc = document.getElementById("nombre");
+let apellidoSc = document.getElementById("apellido");
+let emailSc = document.getElementById("email");
+let paisSc = document.getElementById("pais");
+let comentariosSc = document.getElementById("comentarios");
+var form = document.getElementById("formulario");
+function handleForm(event) {
+  event.preventDefault();
+}
+if (form != null) {
+  form.addEventListener("submit", handleForm);
+}
+
+let botonContacto = document.getElementById("open-btn");
+let modal = document.getElementById("modal-formulario");
+let backdrop = document.getElementById("backdrop");
+let botonCerrar = document.getElementById("close-btn");
+cerrarModal();
+setUpButtons();
+
+function setUpButtons() {
+  window.onclick = function (event) {
+    if (event.target == document.getElementById("backdrop")) {
+      cerrarModal();
+    }
+  };
+  if (botonCerrar != null && botonContacto != null) {
+    botonCerrar.onclick = cerrarModal;
+    botonContacto.onclick = abrirModal;
+  }
+
+}
+function cerrarModal() {
+  modal?.classList.add("esconder-modal");
+  limpiarCampos();
+}
+
+function abrirModal() {
+  modal?.classList.remove("esconder-modal");
+}
+
+function limpiarCampos() {
+  nombreSc.value = "";
+  apellidoSc.value = "";
+  comentariosSc.value = "";
+  emailSc.value = "";
+  paisSc.value = "";
+}
